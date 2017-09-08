37546048f7ce62f1c516ef2c27e87c835f40dd93
Enrique Becerra <kabeza@gmail.com>
2017-09-08 10:40:18 -0300

    Ejercicio 2 SPA con error en mouseover en las imagenes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent  } from './components/heroes/heroes.component';
+
+const APP_ROUTES: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'acerca', component: AboutComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '' }
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
